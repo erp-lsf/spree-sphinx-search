3d866377f5dd062b646708a1a39c6aa0b9ca9fab
Sergey Polygalin <sevil89@gmail.com>
2014-04-01 11:35:26 +0400

    Add dependency spree_core >= 2.2.0 Change folder structure

---
+++ lib/spree_sphinx_search/engine.rb
@@ -0,0 +1,26 @@
+module SpreeSphinxSearch
+  class Engine < Rails::Engine
+    require 'spree/core'
+    isolate_namespace Spree
+    engine_name 'spree_sphinx_search'
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    def self.activate
+      # if Spree::Config.instance
+      #   Spree::Config.searcher_class = Spree::Search::ThinkingSphinx
+      #   Spree::Config.set(:product_price_ranges => 
+      #                 ["Under $25", "$25 to $50", "$50 to $100", "$100 to $200", "$200 and above"])
+      # end
+
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.env == "production" ? require(c) : load(c)
+      end
+    end
+    
+    config.to_prepare &method(:activate).to_proc
+    
+    def load_tasks
+    end
+  end
+end
