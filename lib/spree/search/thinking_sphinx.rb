ec85c14e36345a120b64dcfac4053e56990f29ed
Sergey Polygalin <sevil89@gmail.com>
2014-04-02 14:10:16 +0400

    Fix typo Remove methods that's not overridden

--- lib/spree/search/thinking_sphinx.rb
+++ lib/spree/search/thinking_sphinx.rb
@@ -1,25 +1,16 @@
 module Spree::Search
   class ThinkingSphinx < Spree::Core::Search::Base
-    def retrieve_products
-      @products_scope = get_base_scope
-      curr_page = page || 1
-
-      @products = @products_scope.includes([:master => :prices])
-      unless Spree::Config.show_products_without_price
-        @products = @products.where("spree_prices.amount IS NOT NULL").where("spree_prices.currency" => current_currency)
-      end
-      @products = @products.page(curr_page).per(per_page)
-    end
 
     protected
 
       def get_base_scope
         ts_base_scope = Spree::Product.ts_is_active
-        ts_base_scope = base_scope.ts_in_taxon(taxon) unless taxon.blank?
+        ts_base_scope = ts_base_scope.ts_in_taxon(taxon) unless taxon.blank?
         base_scope = get_products_conditions_for(ts_base_scope, keywords)
+        base_scope = add_search_scopes(base_scope)
         base_scope
       end
-
+     
       # method should return new scope based on base_scope
       def get_products_conditions_for(ts_base_scope, query)
         unless query.blank?
