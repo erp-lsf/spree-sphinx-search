ddf4efe3ada2384bd7b6acb5e754c375c475e9a8
Sergey Polygalin <sevil89@gmail.com>
2014-04-30 18:25:01 +0400

    Add per page option

--- lib/spree/search/thinking_sphinx.rb
+++ lib/spree/search/thinking_sphinx.rb
@@ -33,9 +33,9 @@ module Spree::Search
       # method should return new scope based on base_scope
       def get_products_conditions_for(ts_base_scope, query)
         unless query.blank?
-          ts_base_scope = ts_base_scope.search_for_ids(query)
+          ts_base_scope = ts_base_scope.search_for_ids(query, per_page: 10000)
         end
-        Spree::Product.where("spree_products.id IN (?)", ts_base_scope.search_for_ids)
+        Spree::Product.where("spree_products.id IN (?)", ts_base_scope.search_for_ids(per_page: 10000))
       end
 
       def prepare(params)
