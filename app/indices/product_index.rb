2dcb5a18c7a8e9c7a041bf50cfa88c35299be71f
Sergey Polygalin <sevil89@gmail.com>
2014-05-05 15:29:06 +0400

    Realtime index instead of delta

--- app/indices/product_index.rb
+++ app/indices/product_index.rb
@@ -1,19 +1,14 @@
-ThinkingSphinx::Index.define 'spree/product', :with => :active_record, :delta => ThinkingSphinx::Deltas::DefaultDelta do
-  is_active_sql = "(spree_products.deleted_at IS NULL AND spree_products.available_on <= NOW())"
+ThinkingSphinx::Index.define 'spree/product', :with => :real_time do
 
   indexes :name
   indexes :description
   indexes :meta_description
   indexes :meta_keywords
   indexes taxons.name, :as => :taxon, :facet => true
-
-  has is_active_sql, :as => :is_active, :type => :boolean
+  has is_active, :type => :boolean
   has master.default_price.amount, as: :product_price, type: :float
-
   if Spree::Price.column_names.include? "old_amount"
-  	has master.default_price.old_amount, as: :product_old_price, type: :float
+    has master.default_price.old_amount, as: :product_old_price, type: :float
   end
-
-  has taxons(:id), :as => :taxon_ids
-  set_property :delta => ThinkingSphinx::Deltas::DefaultDelta
+  has taxon_ids, :as => :taxon_ids, type: :integer, multi: true
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -5,12 +5,16 @@ module Spree
     class_attribute :indexed_options
     self.indexed_options = []
 
-    sphinx_scope(:ts_is_active) do 
-    	{ with: { is_active: true } }
+    sphinx_scope(:ts_is_active) do
+      { with: { is_active: true } }
     end
 
     sphinx_scope(:ts_in_taxon) do |taxon|
     	{ with: { taxon_ids: taxon.self_and_descendants.pluck(:id) } }
+    end
+
+    def is_active
+      deleted_at == nil && available_on < Time.now
     end
 
   end
