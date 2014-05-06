c240da179e2f2f783bcd45a5805580c1d01da8e4
Sergey Polygalin <sevil89@gmail.com>
2014-05-06 09:44:34 +0400

    New hash style

--- app/indices/product_index.rb
+++ app/indices/product_index.rb
@@ -1,14 +1,15 @@
-ThinkingSphinx::Index.define 'spree/product', :with => :real_time do
+ThinkingSphinx::Index.define 'spree/product', with: :real_time do
 
   indexes :name
   indexes :description
   indexes :meta_description
   indexes :meta_keywords
-  indexes taxons.name, :as => :taxon, :facet => true
-  has is_active, :type => :boolean
+  indexes taxons.name, as: :taxon, facet: true
+
+  has is_active, type: :boolean
   has master.default_price.amount, as: :product_price, type: :float
   if Spree::Price.column_names.include? "old_amount"
     has master.default_price.old_amount, as: :product_old_price, type: :float
   end
-  has taxon_ids, :as => :taxon_ids, type: :integer, multi: true
+  has taxon_ids, as: :taxon_ids, type: :integer, multi: true
 end
